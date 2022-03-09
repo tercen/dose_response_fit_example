d703d23ec1174f474e98cc0df137d29d0184cb1f
Thiago Monteirio <tsmonteiro@gmail.com>
2022-03-09 13:57:33 +0000

    Operator info

--- README.md
+++ README.md
@@ -1,38 +1,21 @@
-# Template R operator
-
-The `Template R operator` is a template repository for the creation of R operators in Tercen. An overview of steps for developing an operator are:
-
-1. create a github repo
-2. install tercen_studio
-3. login to tercen_studio
-4. git clone the newly created repo
-5. start developing in R in tercen_studio
-6. add R packages to the repo
-7. push to the github repo
-8. go to tercen and install the operator
-
-More information can be found in [Tercen app builder's guide](https://tercen.github.io/appbuilders-guide/).
-
-Below is the operator README standard structure:
-
 ##### Description
 
-The `Template R operator` is a template repository for the creation of R operators in Tercen.
+Fits a non-linear dose-response curve using the 4PL or 5Pl models as implemented by either `nplr` or `drda` packages in R.
 
 ##### Usage
 
 Input projection|.
 ---|---
-`x-axis`        | type, description 
-`y-axis`        | type, description 
-`row`           | type, description 
-`column`        | type, description 
-`colors`        | type, description 
-`labels`        | type, description 
+`x-axis`        | numeric (log10), Concentration
+`y-axis`        | numeric, Dose response
+`row`           | 'Assay ID' (mandatory)
+`colors`        | char, 'Sample type', Binary, QC or Standard
+
 
 Input parameters|.
 ---|---
-`input_var`        | parameter description
+`Library`        | Either nplr or drda: library used to perform curve fitting
+`Model`          | Either 4PL or 5PL: Number of parameters in the model used to fit
 
 Output relations|.
 ---|---
@@ -45,6 +28,6 @@ Details on the computation.
 
 ##### See Also
 
-[template_shiny_operator](https://github.com/tercen/template_shiny_operator)
-, [template_docker_operator](https://github.com/tercen/template_docker_operator)
+[nonlinear_regression_operator](https://github.com/tercen/nonlinear_regression_operator)
+
 
--- main.R
+++ main.R
@@ -1,11 +1,144 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
 
+library(drda)
+library(nplr)
+
+
+.drda_fit <- function(df){
+  x <- df$.x
+  y <- df$.y
+  
+  maxY <- max(y)
+  y <- y / maxY
+  
+  stdX <- x[df$`Sample type` == 'Standard']
+  stdY <- y[df$`Sample type` == 'Standard']
+  
+  qcX <- x[df$`Sample type` == 'QC']
+  qcY <- y[df$`Sample type` == 'QC']
+  
+  
+  
+  mdl <- drda(y ~ x, data = data.frame( x=stdX, y=stdY ), 
+              mean_function = 'logistic5',
+              is_log=TRUE)
+  
+  
+  w <- 1 / ( (stdY)**2  )
+  mdlWeight <- drda(y ~ x, data = data.frame( x=stdX, y=stdY ), 
+                    mean_function = 'logistic5',
+                    weights = w, 
+                    is_log=TRUE)
+  
+  coeff <- unlist( mdl$coefficients, use.names = FALSE)
+  
+  qcYp <- ((coeff['alpha'] + (coeff['beta'] -coeff['alpha']  ) / 
+              (1 + coeff['nu'] * exp(-coeff['eta'] * (qcX - coeff['phi'])))^(1 / coeff['nu'])  ) ) * maxY
+  
+  coeff <- unlist( mdlWeight$coefficients, use.names = FALSE)
+  qcYwp <- ((coeff['alpha'] + (coeff['beta'] -coeff['alpha']  ) / 
+               (1 + coeff['nu'] * exp(-coeff['eta'] * (qcX - coeff['phi']) ))^(1 / coeff['nu'])  ) ) * maxY
+  
+  rowIdx <- rep( unique(df$.ri)[1], length(qcYp) )
+  colIdx <- rep( unique(df$.ci)[1], length(qcYp) )
+  
+  outDf <- data.frame(
+    .ri=rowIdx,
+    .ci=colIdx,
+    responseU=qcYp,
+    responseW=qcYwp,
+    logConcentration=qcX,
+    diff=(1-(qcYwp/qcYp))*100,
+    npar=5
+  ) 
+  
+  return(outDf)
+}
+
+.nplr_fit <- function(df){
+  x <- df$.x
+  y <- df$.y
+  
+  maxY <- max(y)
+  y <- y / maxY
+  
+  stdX <- x[df$`Sample type` == 'Standard']
+  stdY <- y[df$`Sample type` == 'Standard']
+  
+  qcX <- x[df$`Sample type` == 'QC']
+  qcY <- y[df$`Sample type` == 'QC']
+  
+  # formula y = B + (T-B) / (1 + exp(b*(xmid-x)))^S
+  # where B and T are the bottom and top asymptotes, and b, xmid and s are the Hill slope, the x-coordinate
+  # at the inflexion point and an asymetric coefficient, respectively.
+  
+  mdlU <- nplr(stdX, stdY, npars=5, useLog=FALSE, silent = TRUE)
+  
+  coeff <- getPar(mdlU  )$params
+  
+  qcYp <- (coeff[['bottom']] + 
+             (coeff[['top']] - coeff[['bottom']])/
+             ((1 + exp(coeff[['scal']]*(coeff[['xmid']]-qcX) ) )^coeff[['s']])) * maxY
+  
+  
+  #getFitValues(mdlU)
+  # getInflexion(mdlU)
+  mdlW <- nplr(stdX, stdY, npars=5, useLog=FALSE, silent = TRUE,
+               method='gw', LPweight=2)
+  
+  qcYwp <- (coeff[['bottom']] + 
+              (coeff[['top']] - coeff[['bottom']])/
+              ((1 + exp(coeff[['scal']]*(coeff[['xmid']]-qcX) ) )^coeff[['s']])) * maxY
+  
+  rowIdx <- rep( unique(df$.ri)[1], length(qcYp) )
+  colIdx <- rep( unique(df$.ci)[1], length(qcYp) )
+  
+  outDf <- data.frame(
+    .ri=rowIdx,
+    .ci=colIdx,
+    responseU=qcYp,
+    responseW=qcYwp,
+    logConcentration=qcX,
+    diff=(1-(qcYwp/qcYp))*100,
+    npar=5
+  ) 
+  
+  return(outDf)
+}
+
+do.curvefit <- function(df, lib){
+  
+  if( lib == 'drda'){
+    outDf <- .drda_fit(df)
+  }else if( lib == 'nplr' ){
+    outDf <- .nplr_fit(df)
+  }
+  
+  
+  return(outDf)
+}
+
+
 ctx = tercenCtx()
 
+rowNames <- ctx$rnames
+colorNames <- ctx$colors
+
+if( !("Assay ID" %in% rowNames) ){
+  error("Row 'Assay ID' is mandatory."  )
+}
+
+if( !("Sample type" %in% colorNames) ){
+  error("Color 'Sample type' is mandatory."  )
+}
+
+
+lib <- 'nplr'
+
 ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+  select( .y, .x, .ci, .ri, 'Sample type'  ) %>%
+  group_by(.ri) %>%
+  do( do.curvefit(., lib) ) %>%
   ctx$addNamespace() %>%
   ctx$save()
