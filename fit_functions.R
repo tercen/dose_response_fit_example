54d2c5d9cb1c3f59137815f144e21ff776054e5c
Thiago Monteiro <tsmonteiro@gmail.com>
2022-07-11 18:09:16 +0000

    refactor: Test data rebuild

--- fit_functions.R
+++ fit_functions.R
@@ -1,7 +1,83 @@
+library(tercen)
+library(tercenApi)
+library(dplyr, warn.conflicts = FALSE)
+
 library(drda)
 library(nplr)
 
-
+curvefit <- function( ctx, lib='nplr' ){
+  rowNames <- ctx$rnames
+  
+  cnames.with.ns <- ctx$cnames
+  
+  
+  cnames.without.ns <- unlist(lapply( cnames.with.ns, function(x) {
+    if( !startsWith(x, '.') ){
+      x<-strsplit(x, '\\.')[[1]][2]
+    }else{
+      x
+    }
+  }))
+  
+  
+  colorNames <- ctx$colors
+  
+  if( !("Assay ID" %in% rowNames) ){
+    stop("Row 'Assay ID' is mandatory."  )
+  }
+  
+  
+  if( !("log" %in% cnames.without.ns) ){
+    stop("Column 'log' is mandatory."  )
+  }
+  
+  if( !("Sample type" %in% colorNames) ){
+    stop("Color 'Sample type' is mandatory."  )
+  }
+  
+  # Read in operator parameters
+  operatorProps <- ctx$query$operatorSettings$operatorRef$propertyValues
+  
+  for( prop in operatorProps ){
+    if (prop$name == "Fitting Library"){
+      lib <- prop$value
+    }
+  }
+  
+  
+  logCol <- ctx$cselect() 
+  names(logCol) <- cnames.without.ns
+  logCol <- mutate(logCol, .ci=seq(0,nrow(logCol)-1))
+  
+  res <- ctx %>%  
+    select( .y, .ci, .ri, 'Sample type'  ) %>%  
+    left_join(logCol, by=".ci") %>%
+    group_by(.ri) %>%
+    do( do.curvefit(., lib ) ) %>%
+    ctx$addNamespace() 
+  
+  
+  colNames <- names(res)
+  
+  # NOTE
+  # do() yields an error when returning a list of data frames.
+  # To circumvent that, the resulting tables are merged with cbind(), which duplicates the rows in the sumamry table
+  # This issue is addressed by issuing an unique id as the summary's first column (summary_index)
+  # The code below separates the table and removes duplicates from the results
+  
+  # NOTE 2
+  # The amount of columns is hard-coded. 
+  # IF something changes in the resulting tables, these numbers need to be changed here as well
+  summaryDf <- res %>% select(append('.ri', colNames[11:length(colNames)] ) ) 
+  summaryDf <- summaryDf[!duplicated(summaryDf[,colNames[11]]),] %>% 
+    select(-colNames[11])
+  
+  dataDf <- res %>% select( colNames[1:10]  ) 
+  
+  result = OperatorResult$new()
+  result$tables = list(tercen::dataframe.as.table(dataDf), tercen::dataframe.as.table(summaryDf))
+  return(result)
+}
 
 .drda_fit <- function(df, npar=5, summary_index=1){
   x <- df$log
@@ -108,8 +184,6 @@ library(nplr)
   
   return( list(outDf, sumDf) )
 }
-
-
 
 
 .nplr_fit <- function(df, npar=5, summary_index=0){
--- workspace.R
+++ workspace.R
@@ -1,10 +1,8 @@
 library(tercen)
-library(dplyr, warn.conflicts = FALSE)
+library(tercenApi)
 
-library(drda)
-library(nplr)
 
-# http://127.0.0.1:5402/admin/w/22ae949dc1a3dd3daf96768225009600/ds/d72f5099-6ecf-4944-8f33-99d0ef0e8909
+#http://127.0.0.1:5402/admin/w/22ae949dc1a3dd3daf96768225009600/ds/d72f5099-6ecf-4944-8f33-99d0ef0e8909
 options("tercen.workflowId" = "22ae949dc1a3dd3daf96768225009600")
 options("tercen.stepId"     = "d72f5099-6ecf-4944-8f33-99d0ef0e8909")
 
@@ -17,76 +15,6 @@ source( 'fit_functions.R' )
 # =======================================================
 ctx = tercenCtx()
 
-rowNames <- ctx$rnames
-
-cnames.with.ns <- ctx$cnames
-cnames.without.ns <- unlist(lapply( cnames.with.ns, function(x) {
-  if( !startsWith(x, '.') ){
-    x<-strsplit(x, '\\.')[[1]][2]
-  }else{
-    x
-  }
-}))
-
-colorNames <- ctx$colors
-
-if( !("Assay ID" %in% rowNames) ){
-  stop("Row 'Assay ID' is mandatory."  )
-}
-
-
-if( !("log" %in% cnames.without.ns) ){
-  stop("Column 'log' is mandatory."  )
-}
-
-if( !("Sample type" %in% colorNames) ){
-  stop("Color 'Sample type' is mandatory."  )
-}
-
-# Read in operator parameters
-lib  <- 'nplr'
-
-operatorProps <- ctx$query$operatorSettings$operatorRef$propertyValues
-
-for( prop in operatorProps ){
-  if (prop$name == "Fitting Library"){
-    lib <- prop$value
-  }
-}
-
-
-
-logCol <- ctx$cselect() 
-names(logCol) <- cnames.without.ns
-logCol <- mutate(logCol, .ci=seq(0,nrow(logCol)-1))
-
-res <- ctx %>%  
-    select( .y, .ci, .ri, 'Sample type'  ) %>%  
-    left_join(logCol, by=".ci") %>%
-    group_by(.ri) %>%
-    do( do.curvefit(., lib ) ) %>%
-    ctx$addNamespace() 
-
-
-colNames <- names(res)
-
-# NOTE
-# do() yields an error when returning a list of data frames.
-# To circumvent that, the resulting tables are merged with cbind(), which duplicates the rows in the sumamry table
-# This issue is addressed by issuing an unique id as the summary's first column (summary_index)
-# The code below separates the table and removes duplicates from the results
-
-# NOTE 2
-# The amount of columns is hard-coded. 
-# IF something changes in the resulting tables, these numbers need to be changed here as well
-summaryDf <- res %>% select(append('.ri', colNames[11:length(colNames)] ) ) 
-summaryDf <- summaryDf[!duplicated(summaryDf[,colNames[11]]),] %>% 
-            select(-colNames[11])
-
-dataDf <- res %>% select( colNames[1:10]  ) 
-
-result = OperatorResult$new()
-result$tables = list(tercen::dataframe.as.table(dataDf), tercen::dataframe.as.table(summaryDf))
-
+result <- curvefit( ctx )
 ctx$save( result )
 
